a01ce7569933190ebe8d8eb936a9904ec0a32951
Chris Blaise <cblaise@untangle.com>
2022-11-03 15:18:55 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 """setup.py"""
 
+import sys
+
 from setuptools import setup
 from subprocess import check_output
 from os.path import isdir
 
-if isdir("../.git"): # debian source tarballs don't contain .git
+if isdir("../.git") or isdir(".git"): # debian source tarballs don't contain .git
     version_cmd = "git describe --tags --always --long"
     version = check_output(version_cmd.split(" ")).decode().strip()
-    with open('runtests/version.py', 'w') as f:
+    print("Working on git version {}".format(version))
+    # enforce https://www.python.org/dev/peps/pep-0440
+    items = version[1:].split('-')
+    if len(items) == 3:
+        version = '{}+{}'.format(items[0], items[2][1:])
+    print("--> PEP-0440 version will be {}".format(version))
+    with open('sync/version.py', 'w') as f:
         f.write('__version__ = "{}"\n'.format(version))
 else:
     version = "undefined"
@@ -17,9 +25,9 @@ setup(name='runtests',
       version=version,
       description='Runtests.',
       long_description='''Runtests provides a runtest binary to run python unittest suites.''',
-      author='Dirk Morris & Untangle.',
-      author_email='dmorris@untangle.com',
-      url='https://untangle.com',
+      author='Chris Blaise',
+      author_email='cblaise@ariata.com',
+      url='https://arista.com',
       scripts=['bin/runtests'],
       data_files=[('/usr/lib/runtests',['files/test_shell.key','files/test_shell.pub'])],
       packages=['runtests'],
