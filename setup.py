5a8e6a7e956f254b8d850da4a14d2b14612bb333
Chris Blaise <cblaise@untangle.com>
2022-11-03 16:10:40 -0400

    Add comma.

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(name='runtests',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7'
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.9'
       ))
